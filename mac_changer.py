1fabc06c3cf5408b98b446dadaf2bef390e7a809
root <root@devinex.devinex>
2020-07-02 03:31:49 -0400

    add raw_input

--- mac_changer.py
+++ mac_changer.py
@@ -7,8 +7,6 @@ new_mac = raw_input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 print("*"*100)
-subprocess.call("ifconfig eth0", shell=True)
 subprocess.call("ifconfig "+ interface +" down", shell=True)
 subprocess.call("ifconfig "+ interface + " hw ether " + new_mac, shell=True)
 subprocess.call("ifconfig "+ interface +" up", shell=True)
-subprocess.call("ifconfig eth0", shell=True)
