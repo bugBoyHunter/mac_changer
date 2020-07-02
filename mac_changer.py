fba895a39a77751e087b7093c3195e084b087b58
root <root@devinex.devinex>
2020-07-02 04:04:05 -0400

    add parser

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,23 @@
 #!/usr/bin/ python
 #ether 00:0c:29:44:03:bc
 import subprocess
+import optparse
 
-interface = raw_input("interface > ")
-new_mac = raw_input("new MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+
+(options, arguments) = parser.parse_args()
+
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 print("*"*100)
-subprocess.call("ifconfig "+ interface +" down", shell=True)
-subprocess.call("ifconfig "+ interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig "+ interface +" up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
