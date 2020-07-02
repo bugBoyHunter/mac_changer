6d6328be77495cff3fec17e97c536c1e83707ab5
root <root@devinex.devinex>
2020-07-02 03:29:00 -0400

    add raw_input

--- mac_changer.py
+++ mac_changer.py
@@ -2,11 +2,11 @@
 #ether 00:0c:29:44:03:bc
 import subprocess
 
-interface = "eth0"
-new_mac = "00:11:22:44:33:55"
+interface = raw_input("interface > ")
+new_mac = raw_input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
-print("*"*50)
+print("*"*100)
 subprocess.call("ifconfig eth0", shell=True)
 subprocess.call("ifconfig "+ interface +" down", shell=True)
 subprocess.call("ifconfig "+ interface + " hw ether " + new_mac, shell=True)
