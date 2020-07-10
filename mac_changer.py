35f919c83b33afbc4155f75188c76165300c70c2
Devin <maverickdev7@gmail.com>
2020-07-10 02:13:43 -0400

    finish up

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,21 @@
-#!/usr/bin/ python
+#!/usr/bin/python
 #ether 00:0c:29:44:03:bc
+
 import subprocess
 import optparse
+import re
+
+def get_arguments():
+    # instructions
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface use --help for more info")
+    elif not options.new_mac: 
+         parser.error("[-] Please specify an new mac use --help for more info")
+    return options
 
 #mac changer
 def change_mac(interface, new_mac):
@@ -11,17 +25,26 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-# instructions
-parser = optparse.OptionParser()
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-
-(options, arguments) = parser.parse_args()
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read the mac address")
 
 #call function
+options = get_arguments()
+curent_mac = get_current_mac(options.interface)
+
+print("Current MAC = " + str(curent_mac))
+
 change_mac(options.interface, options.new_mac)
 
+curent_mac = get_current_mac(options.interface)
 
-
-
+if curent_mac == options.new_mac:
+    print("[+] Mac address was succesfully changed to " + curent_mac)
+else:
+    print("[-] Mac address did not get changed")
